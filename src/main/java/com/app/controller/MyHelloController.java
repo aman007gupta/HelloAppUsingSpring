8d089efca87f3249a3dfffd5dc9832ae638536f0
aman007gupta <aman9453122969@gmail.com>
2021-01-16 12:03:36 +0530

    [Aman]Added: code to make rest call to show hello name

--- src/main/java/com/app/controller/MyHelloController.java
+++ src/main/java/com/app/controller/MyHelloController.java
@@ -1,6 +1,8 @@
 package com.app.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,8 @@ public class MyHelloController {
         return "Hello From Aman!!";
     }
 
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
 }
