f2a689af53575e362fe46c6e13ff0a0b38d85ba0
aman007gupta <aman9453122969@gmail.com>
2021-01-16 12:10:15 +0530

    [Aman]Added: code to demonstrate rest call using get request and pass name as path variable

--- src/main/java/com/app/controller/MyHelloController.java
+++ src/main/java/com/app/controller/MyHelloController.java
@@ -1,9 +1,6 @@
 package com.app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -16,6 +13,11 @@ public class MyHelloController {
 
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 }
